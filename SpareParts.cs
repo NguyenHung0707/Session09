375e152d4bd107c27a1fe85219ab21393be04022
NguyenHung0707 <111960391+NguyenHung0707@users.noreply.github.com>
2023-05-25 11:08:29 +0700

    session09

---
+++ SpareParts.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Automotive
+{
+    public class SpareParts
+    {
+        String spareName;
+            public SpareParts() {
+            spareName = "Gear Box";
+            }
+        public void Display()
+        {
+            Console.WriteLine("SpareParts Name : " + spareName);
+        }
+    }
+}
